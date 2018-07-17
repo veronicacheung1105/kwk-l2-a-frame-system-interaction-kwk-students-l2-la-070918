79f120162ca6c1b9ee9f396168f4d6d48c176a1e
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-17 23:02:04 +0000

    automatically backed up by learn

--- systems/Game.js
+++ systems/Game.js
@@ -72,8 +72,8 @@ AFRAME.registerSystem('game', {
 
   newBallShouldSpawn: function() {
     // TODO: implement me!
-    for (var count = 0; count < 50; count++) while {
-      
+    while (var count = 0; count < 50; count++) {
+          this.GAME.spawnBall()
     }
     // hint: Is there some way to use Date.now() to compare the current time and the time since the last ball spawned?
     return false
